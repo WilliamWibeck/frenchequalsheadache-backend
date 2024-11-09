ebef3af7b9ccf265e3ff6304c45eb7068e5b1a97
WilliamWibeck <william.wibeck@gmail.com>
2024-11-09 20:50:14 +0100

    Base structure spring

---
+++ src/main/java/com/frenchequalsheadache/FrenchequalsheadacheApplication.java
@@ -0,0 +1,13 @@
+package com.frenchequalsheadache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrenchequalsheadacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrenchequalsheadacheApplication.class, args);
+	}
+
+}
